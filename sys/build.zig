c9f58668742199a2d227e5f07384730cd8cf2bd0
Pekka Enberg <penberg@iki.fi>
2023-06-03 13:30:45 +0300

    Disable FP and Neon CPU features
    The kernel is not prepared to deal with them...
    
    Suggested by "experienced corner" on Zig Discord.

--- sys/build.zig
+++ sys/build.zig
@@ -1,10 +1,16 @@
 const Builder = @import("std").build.Builder;
 const CrossTarget = @import("std").zig.CrossTarget;
+const Feature = @import("std").Target.Cpu.Feature;
 const Target = @import("std").Target;
 
 pub fn build(b: *Builder) void {
+    const features = Target.aarch64.Feature;
+    var cpu_features_disable = Feature.Set.empty;
+    cpu_features_disable.addFeature(@enumToInt(features.fp_armv8));
+    cpu_features_disable.addFeature(@enumToInt(features.neon));
     const target = CrossTarget{
         .cpu_arch = Target.Cpu.Arch.aarch64,
+        .cpu_features_sub = cpu_features_disable,
         .os_tag = Target.Os.Tag.freestanding,
         .abi = Target.Abi.none,
     };
